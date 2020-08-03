099c99963bdcc42aaa43875a45abcfaf663fe1e4
Yue Su <su2047@gmail.com>
2020-08-03 17:32:10 -0300

    added sumbit and clear the task function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React from 'react';
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
+import { v4 as uuid } from 'uuid';
+
+const initialTaskList = []
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super()
+
+    this.state = {
+      taskList: initialTaskList,
+    }
+  }
+
+  submit = (name) => {
+    const newTask = {
+      taskName: name,
+      id: uuid(),
+      completed: false,
+    }
+
+    this.setState({
+      taskList: [...this.state.taskList, newTask],
+    })
+
+    //console.log(this.state.taskList)
+  }
+
+  clearTask = () => {this.setState({taskList: initialTaskList})}
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList taskList={this.state.taskList} />
+        <TodoForm submit={this.submit} />
+        <button onClick={this.clearTask}>Clear the Task</button>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/TodoForm.js
@@ -0,0 +1,43 @@
+import React from "react"
+
+class TodoForm extends React.Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            taskName : ''
+        }
+    }
+
+    inputHandler = event => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+        //console.log(this.state)
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+        this.props.submit(this.state.taskName)
+    }
+    
+  render() {
+      return <div>
+          <form onSubmit={this.submitHandler}>
+              <label>
+                  <h4>Task Name:</h4>
+                  <input
+                      type="text"
+                      name='taskName'
+                      value={this.state.taskName}
+                      onChange={this.inputHandler}
+                  />
+              </label>
+              <button>Submit</button>
+        </form>
+    </div>
+  }
+}
+
+export default TodoForm
