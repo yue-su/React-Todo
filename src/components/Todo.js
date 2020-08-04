0bb4647cd713a13b78165f81cea247f0cc807b34
Yue Su <su2047@gmail.com>
2020-08-04 15:01:54 -0300

    fixed the typography

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Checkbox from "@material-ui/core/Checkbox"
-import ListItem from '@material-ui/core/ListItem';
+import ListItem from '@material-ui/core/ListItem'
+import Typography from "@material-ui/core/Typography"
 
 class Todo extends React.Component{
     render() {
@@ -10,7 +11,7 @@ class Todo extends React.Component{
               onClick={() => this.props.toggle(this.props.item.id)}
               type="checkbox"
             />
-            {this.props.item.taskName}
+            <Typography>{this.props.item.taskName}</Typography>
           </ListItem>
         )
     }
